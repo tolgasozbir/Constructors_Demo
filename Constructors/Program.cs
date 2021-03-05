e98e200c76d7345b70e58b42d9b067ea363106ce
Tolga SÖZBİR <tolgasoz1@gmail.com>
2021-03-05 21:12:46 +0300

    Add project files.

---
+++ Constructors/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { Id = 1, FirstName = "Tolga", LastName = "Sözbir", City = "Kocaeli" };
+
+            Customer customer2 = new Customer(2, "Levent", "Sözbir", "Eskişehir");
+            Console.WriteLine(customer2.FirstName);
+
+            Console.ReadKey();
+        }
+    }
+    class Customer
+    {
+        //Default Constructor
+        public Customer()
+        {
+            
+        }
+        //Overload
+        public Customer(int id,string firstName,string lastName,string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
